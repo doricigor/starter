3b0ef91addbce7761b0f1e5088dcfb30cce5e407
Igor Doric <doricigor@yahoo.com>
2020-06-17 18:06:41 +0200

    JS setup

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Starter v1</title>
     <link rel="icon" type="image/png" href="img/favicon.png">
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
 <header>
